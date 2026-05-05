ab16bc7f9732a4829562f60dcad886776455750e
Ananya Raj <rajananya937@gmail.com>
2026-05-05 18:11:12 +0530

    phone 3d model showing issue solved3

--- src/components/Tech.jsx
+++ src/components/Tech.jsx
@@ -7,16 +7,36 @@ import { technologies } from "../constants";
 
 const Tech = () => {
   return (
-    <div className='flex flex-row flex-wrap justify-center gap-10'>
-      {technologies.map((technology) => (
-        <div className='w-20 h-20 sm:w-28 sm:h-28' key={technology.name}>
-          <BallCanvas icon={technology.icon} />
-          <p className="text-center text-secondary text-[14px] mt-2">
-            {technology.name}
-          </p>
-        </div>
-      ))}
-    </div>
+    // <div className='flex flex-row flex-wrap justify-center gap-10'>
+    //   {technologies.map((technology) => (
+    //     <div className='w-20 h-20 sm:w-28 sm:h-28' key={technology.name}>
+    //       <BallCanvas icon={technology.icon} />
+    //       <p className="text-center text-secondary text-[14px] mt-2">
+    //         {technology.name}
+    //       </p>
+    //     </div>
+    //   ))}
+    // </div>
+    <div className='w-20 h-20 sm:w-28 sm:h-28 flex flex-col items-center' key={technology.name}>
+  
+  {/* Desktop → 3D balls */}
+  <div className="hidden sm:block">
+    <BallCanvas icon={technology.icon} />
+  </div>
+
+  {/* Mobile → normal image */}
+  <div className="sm:hidden">
+    <img
+      src={technology.icon}
+      alt={technology.name}
+      className="w-16 h-16 object-contain"
+    />
+  </div>
+
+  <p className="text-center text-secondary text-[14px] mt-2">
+    {technology.name}
+  </p>
+</div>
   )
 }
 
